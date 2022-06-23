cf2e842c9e50197b4e420c2fca9e060a609c11c3
Nikolay010 <timofeev7300@ukr.net>
2022-06-23 17:27:20 +0300

    Cinema: fixed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
-
-  before_action :authenticate_user!
   before_action :set_movie, only: [:show, :edit, :update, :destroy]
 
   def index
